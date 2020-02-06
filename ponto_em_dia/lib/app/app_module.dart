ebdad33fca4d69c8709fe10f380b2fe35df806d1
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-02-06 19:55:06 -0300

    commit

---
+++ ponto_em_dia/lib/app/app_module.dart
@@ -0,0 +1,30 @@
+import 'package:ponto_em_dia/app/app_controller.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter/material.dart';
+
+import '../app/app_widget.dart';
+import '../app/modules/home/home_module.dart';
+
+// import 'modules/home/home_controller.dart';
+// import 'modules/login/login_controller.dart';
+import 'modules/login/login_module.dart';
+
+class AppModule extends MainModule {
+  @override
+  List<Bind> get binds => [
+        Bind((i) => AppController()),
+        // Bind((i) => HomeController()),
+        // Bind((i) => LoginController()),
+      ];
+
+  @override
+  List<Router> get routers => [
+        Router('/', module: HomeModule()),
+        Router('login', module: LoginModule()),
+      ];
+
+  @override
+  Widget get bootstrap => AppWidget();
+
+  static Inject get to => Inject<AppModule>.of();
+}
