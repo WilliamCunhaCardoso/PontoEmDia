9b6905f54a318d1b4b289b0691c27203023b8ce2
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-02-13 11:10:44 -0300

    adicionado expanded

--- ponto_em_dia/lib/app/modules/widgets/custom_card.dart
+++ ponto_em_dia/lib/app/modules/widgets/custom_card.dart
@@ -4,23 +4,37 @@ import 'package:gradient_widgets/gradient_widgets.dart';
 class CustomCard extends StatelessWidget {
   final IconData icon;
   final String text;
+  final int flex;
 
-  const CustomCard({Key key, @required this.icon, @required this.text}) : super(key: key);
+  const CustomCard(
+      {Key key, @required this.icon, @required this.text, @required this.flex})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GradientCard(
-      child: Container(
-        width: 100,
-        height: 100,
-        child: Column(
-          children: <Widget>[
-            Icon(this.icon, size: 50,),
-            Text(this.text, style: TextStyle(),),
-          ],
+    return Expanded(
+      flex: this.flex,
+      child: GradientCard(
+        child: Container(
+          height: 100,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                this.icon,
+                size: 40
+              ),
+              Text(
+                this.text,
+                textAlign: TextAlign.center,
+                style: TextStyle(),
+              ),
+            ],
+          ),
         ),
+        gradient: Gradients.haze,
       ),
-      gradient: Gradients.taitanum,
     );
   }
 }
