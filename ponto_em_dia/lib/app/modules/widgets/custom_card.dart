80d996a23a44dcacce19725b253758874e0030cd
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-02-14 13:46:36 -0300

    correção

--- ponto_em_dia/lib/app/modules/widgets/custom_card.dart
+++ ponto_em_dia/lib/app/modules/widgets/custom_card.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:gradient_widgets/gradient_widgets.dart';
+import 'package:ponto_em_dia/app/modules/historico/historico_controller.dart';
 
 class CustomCard extends StatelessWidget {
   final IconData icon;
   final String text;
   final int flex;
+  final String categoria;
 
-  const CustomCard(
-      {Key key, @required this.icon, @required this.text, @required this.flex})
+  CustomCard(
+      {Key key,
+      @required this.icon,
+      @required this.text,
+      @required this.flex,
+      @required this.categoria})
       : super(key: key);
+
+  var controller = Modular.get<HistoricoController>();
 
   @override
   Widget build(BuildContext context) {
@@ -17,20 +26,23 @@ class CustomCard extends StatelessWidget {
       child: GradientCard(
         child: Container(
           height: 100,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Icon(
-                this.icon,
-                size: 40
-              ),
-              Text(
-                this.text,
-                textAlign: TextAlign.center,
-                style: TextStyle(),
-              ),
-            ],
+          child: GestureDetector(
+            onTap: () {
+              controller.addPonto(this.categoria);
+              print(this.categoria);
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(this.icon, size: 40),
+                Text(
+                  this.text,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(),
+                ),
+              ],
+            ),
           ),
         ),
         gradient: Gradients.haze,
