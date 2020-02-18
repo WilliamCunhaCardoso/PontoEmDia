9bcb653b0b72d4fe19b35af2daa2b06bdcbc4a86
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-02-18 13:54:21 -0300

    import corrigido

--- ponto_em_dia/lib/app/modules/perfil/perfil_page.dart
+++ ponto_em_dia/lib/app/modules/perfil/perfil_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:ponto_em_dia/app/app_controller.dart';
 import 'package:ponto_em_dia/app/shared/widgets/custom_bottombar.dart';
 
 class PerfilPage extends StatefulWidget {
