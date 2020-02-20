1bc4fd4847b2292ee7fd2052ce973912827def54
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-02-20 07:27:17 -0300

    conversão para stfull

--- ponto_em_dia/lib/app/shared/widgets/custom_card.dart
+++ ponto_em_dia/lib/app/shared/widgets/custom_card.dart
@@ -3,7 +3,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:gradient_widgets/gradient_widgets.dart';
 import 'package:ponto_em_dia/app/modules/historico/historico_controller.dart';
 
-class CustomCard extends StatelessWidget {
+class CustomCard extends StatefulWidget {
   final IconData icon;
   final String text;
   final int flex;
@@ -17,27 +17,32 @@ class CustomCard extends StatelessWidget {
       @required this.categoria})
       : super(key: key);
 
+  @override
+  _CustomCardState createState() => _CustomCardState();
+}
+
+class _CustomCardState extends State<CustomCard> {
   var controller = Modular.get<HistoricoController>();
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      flex: this.flex,
+      flex: this.widget.flex,
       child: GradientCard(
         child: Container(
           height: 100,
           child: GestureDetector(
             onTap: () {
-              controller.addPonto(this.categoria);
-              print(this.categoria);
+              controller.addPonto(this.widget.categoria);
+              print(this.widget.categoria);
             },
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Icon(this.icon, size: 40),
+                Icon(this.widget.icon, size: 40),
                 Text(
-                  this.text,
+                  this.widget.text,
                   textAlign: TextAlign.center,
                   style: TextStyle(),
                 ),
