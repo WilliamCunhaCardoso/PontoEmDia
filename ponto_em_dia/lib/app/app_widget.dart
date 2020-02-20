958e68801d0634d977fb8cacccbdec3dae1bb886
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-02-20 07:25:57 -0300

    ok toast wrapping material app

--- ponto_em_dia/lib/app/app_widget.dart
+++ ponto_em_dia/lib/app/app_widget.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:oktoast/oktoast.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      navigatorKey: Modular.navigatorKey,
-      title: 'Ponto Em Dia',
-      theme: ThemeData(
-        primaryColor: Colors.black,
-        cursorColor: Colors.black
+    return OKToast(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        navigatorKey: Modular.navigatorKey,
+        title: 'Ponto Em Dia',
+        theme: ThemeData(primaryColor: Colors.black, cursorColor: Colors.black),
+        initialRoute: 'login',
+        onGenerateRoute: Modular.generateRoute,
       ),
-      initialRoute: 'login',
-      onGenerateRoute: Modular.generateRoute,
     );
   }
 }
