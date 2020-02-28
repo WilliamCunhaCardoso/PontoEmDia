e1a942f61bc3eabf67add32ee72a598b9a9f73a9
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-02-28 10:30:26 -0300

    titulo de bloco e correções

--- ponto_em_dia/lib/app/modules/perfil/perfil_page.dart
+++ ponto_em_dia/lib/app/modules/perfil/perfil_page.dart
@@ -17,37 +17,81 @@ class _PerfilPageState extends State<PerfilPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: GFCard(
-        content: Column(
-          children: <Widget>[
-            GFImageOverlay(
-              height: 100,
-              width: 100,
-              shape: BoxShape.circle,
-              image: NetworkImage(
-                  'https://media-exp1.licdn.com/dms/image/C4E03AQHgDSBFT832lQ/profile-displayphoto-shrink_200_200/0?e=1588204800&v=beta&t=6g3Mbt27TAhyEkxlNwPsPRaK81jjpz2W3rbtlHK123g'),
-            ),
-            GFListTile(
-              titleText: 'Nome',
-              subtitleText: 'William Cunha Cardoso',
-            ),
-            GFListTile(
-              titleText: 'Cargo',
-              subtitleText: 'Desenvolvedor Mobile Jr',
-            ),
-            GFListTile(
-              titleText: 'Departamento',
-              subtitleText: 'Desenvolvimento',
-            ),
-            GFListTile(
-              titleText: 'Supervisor',
-              subtitleText: 'Mailliw Osodrac',
-            ),
-            GFListTile(
-              titleText: 'Matrícula',
-              subtitleText: 'D3V001',
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: GFCard(
+          content: Column(
+            children: <Widget>[
+              GFImageOverlay(
+                height: 100,
+                width: 100,
+                shape: BoxShape.circle,
+                image: NetworkImage(
+                    'https://media-exp1.licdn.com/dms/image/C4E03AQHgDSBFT832lQ/profile-displayphoto-shrink_200_200/0?e=1588204800&v=beta&t=6g3Mbt27TAhyEkxlNwPsPRaK81jjpz2W3rbtlHK123g'),
+              ),
+              GFListTile(
+                title: GFTypography(
+                  text: 'Dados',
+                  type: GFTypographyType.typo2,
+                ),
+              ),
+              GFListTile(
+                titleText: 'Nome',
+                subtitleText: 'William Cunha Cardoso',
+              ),
+              GFListTile(
+                titleText: 'Cargo',
+                subtitleText: 'Desenvolvedor Mobile Jr',
+              ),
+              GFListTile(
+                titleText: 'Departamento',
+                subtitleText: 'Desenvolvimento',
+              ),
+              GFListTile(
+                titleText: 'Supervisor',
+                subtitleText: 'Mailliw Osodrac',
+              ),
+              GFListTile(
+                titleText: 'Matrícula',
+                subtitleText: 'D3V001',
+              ),
+              GFListTile(
+                title: GFTypography(
+                  text: 'Marcações',
+                  type: GFTypographyType.typo2,
+                ),
+              ),
+              GFListTile(
+                title: Text(
+                  'Sincronizados',
+                  style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w500,
+                      color: GFColors.SUCCESS),
+                ),
+                subtitleText: '13',
+              ),
+              GFListTile(
+                title: Text(
+                  'Sincronizados',
+                  style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w500,
+                      color: GFColors.WARNING),
+                ),
+                subtitleText: '4',
+              ),
+              GFListTile(
+                title: Text(
+                  'Perdidos',
+                  style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w500,
+                      color: GFColors.DANGER),
+                ),
+                subtitleText: '3',
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: CustomBottomBar(),
