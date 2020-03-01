fab66126e6a8b4e191c2d4c047fadb9c70131a86
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-03-01 00:24:50 -0300

    commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ponto_em_dia
+
+Um Mockup produzido com base no Modular + Mobx, com propósito de estudo.
+
+## Capturas de Tela
--- lib/app/shared/functions/showToast.dart
+++
@@ -1,9 +0,0 @@
-import 'package:oktoast/oktoast.dart';
-
-toast(String value, int duracao) {
-  return showToast(
-    value,
-    duration: Duration(seconds: duracao),
-    position: ToastPosition.bottom,
-  );
-}
